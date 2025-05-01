5e413d3cad8a107d7bf3fdc9b40915004ae801d2
tunmek <ekairwanto17@gmail.com>
2025-05-01 11:46:35 +0700

    submit tugas ketiga part4

---
+++ quiz/part4/soal3.js
@@ -0,0 +1,10 @@
+let name = "Agus";
+let age = 30;
+let address = "Jln. Malioboro, Yogjakarta";
+let hobby = "gaming";
+let fullSentence = processSentence(name,age,address,hobby);
+
+function processSentence(name,age,address,hobby) {
+    return text = "nama saya " + name + ", umur saya " + age + " tahun, alamat saya di " + address + ", dan saya punya hobby yaitu " + hobby + "!"
+}
+console.log(fullSentence); // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan saya punya hobby yaitu gaming!"
